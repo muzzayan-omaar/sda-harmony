50e1fb560a04a7ecfc4dc01798c5399e9ce4b010
Muzzayan Omaar <muzzayanomaar@gmail.com>
2026-07-06 22:37:26 +0400

    Configure NativeWind and project foundation

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,14 +2,10 @@ import { View, Text } from "react-native";
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>SDA Harmony</Text>
+    <View className="flex-1 bg-blue-950 justify-center items-center">
+      <Text className="text-white text-3xl font-bold">
+        NativeWind Works 🎉
+      </Text>
     </View>
   );
 }
---
+++ mobile/theme/colors.ts
@@ -0,0 +1,5 @@
+const colors = {
+  primary: "#0B3D91",
+};
+
+export default colors;
